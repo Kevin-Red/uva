001d59443d26deb968349c71c3c2dd56111718ed
Kevin-Red <kevinr@foxmail.com>
2019-11-06 10:24:17 +0800

    uva353

--- Uva353/C/main.cpp
+++ Uva353/C/main.cpp
@@ -1,19 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include<iostream>
 #include<algorithm>
 #include<string>
 #include<ctime>
 #include<cstdio>
+#include<set>
+
+
 using namespace std;
-int main(){
-	freopen("date.in","r",stdin);
-	freopen("date.out","w",stdout);
-	long start,finish;
-   	double totaltime;
-   	start=clock();
-	
-	finish=clock();
-  	totaltime=(double)(finish-start)/CLOCKS_PER_SEC;
-   	printf("\n´Ë³ÌÐòµÄÔËÐÐÊ±¼äÎª%.2fÃë",totaltime);
-	return 0;
+
+int main() {
+    freopen("date.in", "r", stdin);
+    freopen("date.out", "w", stdout);
+    long start, finish;
+    double totaltime;
+    start = clock();
+
+    string s;
+
+    while (cin >> s) {
+        string b, e;
+        set<string> book;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            for (int j = 1; j <= len - i; j++) {
+                b = s.substr(i, j);
+                e = b;
+                reverse(e.begin(), e.end());
+                if (b == e) {
+                    book.insert(e);
+                }
+
+            }
+        }
+        cout << "The string '" << s << "' contains " << book.size() << " palindromes." << endl;
+    };
+
+
+    finish = clock();
+    totaltime = (double) (finish - start) / CLOCKS_PER_SEC;
+    printf("\næ­¤ç¨‹åºçš„è¿è¡Œæ—¶é—´ä¸º%.2fç§’", totaltime);
+    return 0;
 }
--- Uva913/C/cmake-build-debug/CMakeFiles/Uva.dir/DependInfo.cmake
+++ Uva913/C/cmake-build-debug/CMakeFiles/Uva.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "D:/UVA/Uva913/C/main.cpp" "D:/UVA/Uva913/C/cmake-build-debug/CMakeFiles/Uva.dir/main.cpp.obj"
+  "F:/github/uva/Uva913/C/main.cpp" "F:/github/uva/Uva913/C/cmake-build-debug/CMakeFiles/Uva.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
