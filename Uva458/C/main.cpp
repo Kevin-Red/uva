030e64f1dda1f0dc81e5339ea13c8cee5ba5e8b6
Kevin-Red <kevinr@foxmail.com>
2019-11-11 23:03:58 +0800

    458

---
+++ Uva458/C/main.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<ctime>
+#include<cstdio>
+using namespace std;
+int main(){
+	freopen("date.in","r",stdin);
+	freopen("date.out","w",stdout);
+	long start,finish;
+   	double totaltime;
+   	start=clock();
+	char c;
+	while ((c = getchar())!= EOF) {
+		putchar(c == '\n' || c == '\r' ? c : c - 7);
+	}
+	finish=clock();
+  	totaltime=(double)(finish-start)/CLOCKS_PER_SEC;
+   	printf("\n此程序的运行时间为%.2f秒",totaltime);
+	return 0;
+}
