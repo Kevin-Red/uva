191ec276d90a1602c2c5b98fc575ea88489cf447
Kevin-Red <kevinr@foxmail.com>
2019-11-11 20:08:42 +0800

    476

---
+++ Uva476/C/cmake-build-debug/CMakeFiles/Uva.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Uva.dir/main.cpp.obj"
+  "Uva.pdb"
+  "Uva.exe"
+  "Uva.exe.manifest"
+  "libUva.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Uva.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
